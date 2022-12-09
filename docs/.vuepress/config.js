3565f9e4c3fba1051a05ddf6d2ff460e0fbb975f
LCai <leancai@126.com>
2022-12-09 21:44:18 +0800

    deploy main branch

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,8 @@
 /*
  * @Author: zhangliangcai leancai@126.com
  * @Date: 2022-12-09 14:57:01
- * @LastEditors: zhangliangcai leancai@126.com
- * @LastEditTime: 2022-12-09 18:35:04
+ * @LastEditors: cai
+ * @LastEditTime: 2022-12-09 21:43:00
  * @FilePath: \VuePressDemo\docs\.vuepress\config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -41,11 +41,11 @@ module.exports = {
               path: "/foo/",
             },
             {
-              title: "one",
+              title: "糟糕的代码",
               path: "/foo/one",
             },
             {
-              title: "two",
+              title: "表设计",
               path: "/foo/two",
             },
           ],
